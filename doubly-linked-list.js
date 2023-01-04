de854d9eb30e95bf59b5e7742b38903c93c045ec
Vedansh Mehra <vedanshofficial@gmail.com>
2023-01-05 01:50:13 +0530

    added get method in double linked list

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -55,6 +55,27 @@ class DoublyLinkedList {
     this.length--;
     return tempHead;
   }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let current;
+    let count;
+    if (index <= this.length / 2) {
+      count = 0;
+      current = this.head;
+      while (count !== index) {
+        current = current.next;
+        count++;
+      }
+    } else {
+      count = this.length - 1;
+      current = this.tail;
+      while (count !== index) {
+        current = current.prev;
+        count--;
+      }
+    }
+    return current;
+  }
 }
 
 const dll = new DoublyLinkedList();
