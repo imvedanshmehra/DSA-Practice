fc8666c089f3854223834c173d9b87b8291b1efe
Vedansh Mehra <vedanshofficial@gmail.com>
2025-12-11 07:46:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 53.9 MB (41.58%)

--- solutions/0035-search-insert-position/solution.js
+++ solutions/0035-search-insert-position/solution.js
@@ -5,21 +5,25 @@
  */
 
 var searchInsert = function(nums, target) {
-    let left = 0
-    let right = nums.length - 1; 
+    let left = 0;
+    let right = nums.length - 1;
     
-    while(left <= right) {
-        const middle = Math.floor((left + right) / 2)
+    while (left <= right) {
+        // Find the middle 
+        let middle = Math.floor((left + right) / 2);
+    
     
         if(target === nums[middle]) {
             return middle
         } else if(target > nums[middle]) {
-            left = middle + 1
+            left = middle + 1;
+        } 
+        else if (target < nums[middle]) {
+            right = middle - 1
         }
-        else {
-            right = middle -1 ;
-        }
+        
     }
-
-    return left
+    
+    return left;
+    
 };
