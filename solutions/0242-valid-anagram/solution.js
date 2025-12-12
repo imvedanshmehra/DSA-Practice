196de082d2548eb43e69d037f0492417f2ec0271
Vedansh Mehra <vedanshofficial@gmail.com>
2025-12-12 20:55:47 +0000

    [LeetCode Sync] Runtime - 16 ms (46.88%), Memory - 61.1 MB (9.84%)

--- solutions/0242-valid-anagram/solution.js
+++ solutions/0242-valid-anagram/solution.js
@@ -4,19 +4,15 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if(s.length !== t.length) return false;
-
-    const tempObj = {}
-
-    for(let val of s) {
-        tempObj[val] = (tempObj[val] || 0) + 1;
+    const obj = {}
+    
+    for(let char of s) {
+        obj[char] = (obj[char] || 0) + 1
     }
-    for(let i =0; i < t.length; i++) {
-        const letter = t[i];
-        
-        if(!tempObj[letter]) return false;
-        tempObj[letter] -= 1
+    
+    for(let char of t) {
+        obj[char] = (obj[char]) - 1
     }
-
-    return true;
+    
+    return Object.values(obj)?.every(val => val === 0)
 };
