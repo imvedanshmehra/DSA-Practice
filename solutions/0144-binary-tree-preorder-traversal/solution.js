60d75c53dcf8c61c1d77a56e91bae08acfc96ce5
Vedansh Mehra <vedanshofficial@gmail.com>
2025-12-28 12:30:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 53.5 MB (69.79%)

---
+++ solutions/0144-binary-tree-preorder-traversal/solution.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    const visited = [];
+
+    function traverse(node) {
+        if(!node) return node;
+
+        visited.push(node.val)
+        if(node.left) traverse(node.left)
+        if(node.right) traverse(node.right)
+
+    } 
+
+    traverse(root);
+    return visited;
+};
