fbcc11b6e6368a8a3d4d50209b3bc492b36b7de4
Vedansh Mehra <vedanshofficial@gmail.com>
2026-01-14 09:30:56 +0000

    [LeetCode Sync] Runtime - 48 ms (36.58%), Memory - 55.8 MB (5.78%)

---
+++ solutions/1894-merge-strings-alternately/solution.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+    let i = 0;
+    let res = '';
+
+    let maxLen = Math.max(word1.length, word2.length)
+
+    while(i<maxLen) {
+        if(word1[i]) {
+            res+=word1[i]
+        }
+        if(word2[i]) {
+            res+=word2[i]
+        }
+        
+        i++;
+    }
+
+    return res
+};
