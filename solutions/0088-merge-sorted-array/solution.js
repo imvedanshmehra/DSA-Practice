aeea6e24d2269b3288f02fef00e60036e828170b
Vedansh Mehra <vedanshofficial@gmail.com>
2026-05-02 09:01:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 54.3 MB (50.74%)

--- solutions/0088-merge-sorted-array/solution.js
+++ solutions/0088-merge-sorted-array/solution.js
@@ -6,17 +6,15 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let i = m-1; // Tracks the last "actual" value of num1
-    let j = n-1; // Tracks the last value of nums2
-    let k = m+n-1 // Tracks the last position where the new value should be entered
+    let i = m-1;
+    let j = n-1;
+    let k = m+n-1;
 
-    // Loop till all the values of num2 are merged into num1
     while(j >= 0) {
-        // Compare i & j and insert the bigger value at the end
         if(nums1[i] > nums2[j]) {
             nums1[k--] = nums1[i--]
         } else {
-            nums1[k--] = nums2[j--]
+            nums1[k--] = nums2[j--];
         }
     }
 };
