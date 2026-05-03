79c7c42952075258c48ebebfc7849e5f1c74e0c5
Vedansh Mehra <vedanshofficial@gmail.com>
2026-05-03 09:57:30 +0000

    [LeetCode Sync] Runtime - 58 ms (46.12%), Memory - 58.4 MB (64.18%)

--- solutions/0080-remove-duplicates-from-sorted-array-ii/solution.js
+++ solutions/0080-remove-duplicates-from-sorted-array-ii/solution.js
@@ -3,20 +3,17 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    let i = 0;
-
     if(nums.length < 2) return nums.length;
-    
 
-    for(let j=1; j < nums.length; j++) {
-        if(nums[i] === nums[j] && nums[i-1] !== nums[i]) {
-            nums[++i] = nums[j]
-        } else if(nums[i] !== nums[j]) {
-            nums[++i] = nums[j]
-        }
+    let i = 2;
+
+    for(let j=2; j < nums.length; j++) {
+        if(nums[j] !== nums[i-2]) {
+            nums[i++] = nums[j];
+        } 
     }
 
-    return i+1;
-
+    return i;
 };
+
 
