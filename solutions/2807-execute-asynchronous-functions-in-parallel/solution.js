0483054a3f8be676d90505818b66b71eb6924b57
Vedansh Mehra <vedanshofficial@gmail.com>
2026-05-08 08:06:49 +0000

    [LeetCode Sync] Runtime - 66 ms (44.27%), Memory - 54.8 MB (37.80%)

---
+++ solutions/2807-execute-asynchronous-functions-in-parallel/solution.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+var promiseAll = function(functions) {
+    return new Promise((resolve, reject) => {
+        let results = [];
+        let completed = 0
+
+        functions.forEach((fn, index) => {
+            fn()
+            .then(res => {
+                results[index] = res;
+                completed++
+
+                if(completed === functions.length) {
+                    resolve(results);
+                }
+            })
+            .catch(err => reject(err))
+        });
+
+    })
+};
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */
