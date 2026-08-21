72b8819519aaf352362d75f4bf812cecb66e9e65
Vedansh Mehra <vedanshofficial@gmail.com>
2026-08-21 09:15:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 55.1 MB (66.50%)

--- solutions/0027-remove-element/solution.ts
+++ solutions/0027-remove-element/solution.ts
@@ -1,11 +1,15 @@
 function removeElement(nums: number[], val: number): number {
+    if(!nums) return;
+
     let i=0;
 
     for(let j=0; j<nums.length; j++) {
-        if(nums[j] !== val) {
-            nums[i++] = nums[j];
+         if(nums[j] !== val) {
+            nums[i] = nums[j];
+            i++
         }
     }
 
     return i;
- };
+};
+
